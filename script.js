ba09e0357fa9fac9f90c74350784fd1c8e6f39cb
itsPrudy <dencecira463@gmail.com>
2024-05-09 10:52:13 +0300

    Finished on making the clickBox functional started on getting the id for player

--- script.js
+++ script.js
@@ -5,7 +5,19 @@ function clickedBox (element){
         element.setAttribute('id', playerSign)
     }else{
         element.innerHTML = `<i class='${playerXIcon}'></i>`
-        players.classList.add('active')
+        players.classList.add('active') //add the sign of the player if the player is active
         element.setAttribute('id', playerSign)
     }
+    selectWinner() //calling the select winner function
+    element.style.pointerEvents = 'none' //once box is clicked u cannot click again
+    playBoard.style.pointerEvents = 'none'//so player cannot automatically click another box until the other player plays
+    let randomTimeDelay = ((Math.random() * 1000) + 200).toFixed() //specified number of decimal places //delay for next player
+    setTimeout(()=>{
+        playBoard.style.pointerEvents = 'auto'
+        playerSign = 'X'
+    }, randomTimeDelay)
+    //once the player has clicked the box, the player will be switched
+}
+function getIdVal(classname){
+    return document.querySelector('.box' + classname).id
 }
