a766d03550757e1919d539803df83b0007a956dd
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2024-05-09 15:15:02 +0300

    added the first few functions of javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+//Selecting all required elements
+
+const selectBox = document.querySelector(".select-box");
+selectXBtn = selectBox.querySelector(".PlayerX"),
+selectOBtn = selectBox.querySelector(".PlayerO"),
+
+  window.onload =() =>{//once window loaded
+    
+  }
