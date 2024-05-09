d7be74b6dc579ad0b88435fd0835d5426570760b
itsPrudy <dencecira463@gmail.com>
2024-05-09 18:39:28 +0300

    Modified the script.js

--- script.js
+++ script.js
@@ -27,19 +27,6 @@ let playerSign = "X";
 let runBot = true; 
 
 // user click function
-function clickedBox(element){
-    if(players.classList.contains("player")){
-        playerSign = "O"; 
-        element.innerHTML = `<i class="${playerOIcon}"></i>`; 
-        players.classList.remove("active"); 
-        element.setAttribute("id", playerSign);
-    }else{
-        element.innerHTML = `<i class="${playerXIcon}"></i>`; 
-        element.setAttribute("id", playerSign); 
-        players.classList.add("active"); 
-    }
-
-
 function clickedBox (element){
     if (players.classList.contains('player')){
         element.innerHTML = `<i class='${playerOIcon}'></i>`
