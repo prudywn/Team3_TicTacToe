f81b2338c8b53f8e1b548516187593b852ebfa7d
itsPrudy <dencecira463@gmail.com>
2024-05-09 19:23:44 +0300

    set the variables to let

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const selectBox = document.querySelector(".select-box"),
-selectBtnX = selectBox.querySelector(".options .playerX"),
-selectBtnO = selectBox.querySelector(".options .playerO"),
-playBoard = document.querySelector(".play-board"),
-players = document.querySelector(".players");
-
+const selectBox = document.querySelector(".select-box")
+let selectBtnX = selectBox.querySelector(".options .playerX")
+let selectBtnO = selectBox.querySelector(".options .playerO")
+let playBoard = document.querySelector(".play-board")
+let players = document.querySelector(".players")
+let allBox = document.querySelectorAll("section span")
+let resultBox = document.querySelector(".result-box")
+let wonText = resultBox.querySelector(".won-text")
+let replayBtn = resultBox.querySelector("button")
 
 window.onload = ()=>{ 
     for (let i = 0; i < allBox.length; i++) { 
