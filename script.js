f0677da7b9fec6b00e044078981d88a9a79ca468
itsPrudy <dencecira463@gmail.com>
2024-05-09 10:20:07 +0300

    Added the cross icon tag inside the clicked ox

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function clickedBox (element){
     if (players.classList.contains('player')){
         element.innerHTML = `<i class='${playerOIcon}'></i>`
-        players.classList.remove('active');
-        element.setAttribute('id', playerSign);
+        players.classList.remove('active')
+        element.setAttribute('id', playerSign)
+    }else{
+        element.innerHTML = `<i class='${playerXIcon}'></i>`
+        players.classList.add('active')
+        element.setAttribute('id', playerSign)
     }
 }
